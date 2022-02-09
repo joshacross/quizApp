bf5759e2803b4a1a7c91fbd498aa5bbd7f52fa55
Killa J <joshuaalexandercross@gmail.com>
2022-02-08 21:31:51 -0600

    setup boilerplate, css, and initial JS functions

---
+++ assets/script.js
@@ -0,0 +1,47 @@
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame);
+
+function startGame () {
+    console.log('start game');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion () {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+
+    }
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    console.log(question);
+}
+
+function selectAnswer () {
+
+}
+
+const questions = [
+    {
+        question: 'What is 2+2?',
+        answers: [
+            {
+                text: '4',
+                correct: true 
+            },
+            {
+                text: '22',
+                correct: false
+            }
+        ]
+    }
+]
