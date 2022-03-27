af0d460f6a53bb91ca16fe1ea869ab482cfd9537
joshacross <joshuaalexandercross@gmail.com>
2022-03-27 00:18:26 -0500

    "updated variables"

--- assets/js/rentcalc.js
+++ assets/js/rentcalc.js
@@ -17,11 +17,15 @@ function returnRent(e) {
     const phone = document.querySelector('#phone').value.trim();
     const rent = document.querySelector('#rent').value.trim();
 
-    rentPayment = rent;
-    nameValue = name;
-    phoneNumber = phone;
+    rent = rentPayment;
+    name = nameValue;
+    phone = phoneNumber;
 
-    console.log(name, phone, rent);
+    // rentPayment = rent;
+    // nameValue = name;
+    // phoneNumber = phone;
+
+    console.log(rentPayment, nameValue, phoneNumber);
 
     getPV();
 }
@@ -33,15 +37,30 @@ function getPV() {
 
 
     alert('CONGRATULATIONS ' + nameValue + '!!! 🎉 🎉 🎉  Paying $' + rentPayment + ' in rent could allow you to purchase a house up to $' + presentValue);
-    savePV(name, phone, presentValue);
+    savePV(name, phone, presentValue, rentPayment);
 }
 
 // save to local storage 
-function savePV(name, phone, presentValue) {
+function savePV(name, phone, presentValue, rent) {
     console.log('save to local storage');
     let passedValue = presentValue;
+    let nameValue = name;
+    let phoneNumber = phone;
+    let rentPayment = rent;
     localStorage.setItem('presentValue', passedValue);
     localStorage.setItem('name', nameValue);
     localStorage.setItem('phone', phoneNumber);
     localStorage.setItem('rent', rentPayment);
 }
+
+// Sending Form Data (POST) with FETCH API
+const userForm = document.getElementById('form');
+
+userForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+
+
+
+});
+
