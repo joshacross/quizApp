ae2fc71dbb54ee3e526c76fc432c5a0d89d22c35
joshacross <joshuaalexandercross@gmail.com>
2022-04-06 18:11:43 -0500

    adding fetch request to db

--- assets/js/displayLockScreen.js
+++ assets/js/displayLockScreen.js
@@ -1,9 +1,9 @@
 //declare variables 
-const displayClicked = document.querySelector('[id*="prp_link"]');
-console.log(displayClicked);
 const showSurvey = document.getElementById('showSurvey');
-const viewDetail = document.getElementsByClassName('view_detail');
-const submitBtn = document.getElementById('start-btn');
+const viewDetail = document.querySelector('[id*="prp_link"]');
+
+const startBtn = document.getElementById('start-btn');
+console.log(startBtn);
 
 let surveyCompleted = Boolean;
 
@@ -12,7 +12,7 @@ let loadStorage = () => {
 
     if (surveyCompleted) {
         console.log('surveySeen');
-        viewDetail.setAttribute('data-trigger-element-class', 'surveyComplete');
+            showSurvey.setAttribute('hidden', true);
         return;
     } else {
         console.log('showSurvey');
@@ -20,16 +20,49 @@ let loadStorage = () => {
     }
 };
 
-submitBtn.addEventListener('click', () => {
-    console.log('surveycomplete');
-    surveyCompleted = true;
-    localStorage.setItem('survey', true);
-    return;
+startBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const emailEl = document.getElementById('email');
+    console.log(emailEl);
+    // if (validateEmail) {
+    //     localStorage.setItem('email', emailEl);
+    //     // submit data to update DB? 
+    //     async function sendForm (url='', data = {}) {
+
+    //         const response = await fetch(url, {
+    //             method: 'POST',
+    //             mode: 'cors',
+    //             cache: 'no-cache',
+    //             credentials: 'omit',
+    //             referrPolicy: 'strict-origin-when-cross-origin',
+    //             body: JSON.stringify(data)
+    //         });
+    //             return response.json();
+    //         }
+          
+    //     sendForm('https://hooks.zapier.com/hooks/catch/9671423/b8up9vj/', emailEl)
+    //         .then(data => {
+    //             console.log(data);
+    //         }).catch((err) => {
+    //             console.log(err);
+    //         });
+    //     hideForm();
+    // } else {
+    //     alert('must enter a valid email address');
+    //     return;
+    // };
 });
 
+let validateEmail = (emailEl) => {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(emailEl).toLowerCase());
+  }
+
+  let hideForm = () => {
+    surveyCompleted = true;
+    localStorage.setItem('survey', true);
+    showSurvey.setAttribute('hidden', true);
+    return;
+}
 
 loadStorage();
-
-// also... 
-
-// I need to set some Params/hashes... So how do we do that... 
