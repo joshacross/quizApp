510cc41e40dc3f991b414fc7ca6385f4536d102c
joshacross <joshuaalexandercross@gmail.com>
2022-04-25 18:53:58 -0500

    debugged console and question iteration

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 
 // Final Complete button to submit Array data
-const submitElement = document.getElementById('submit-btn');
+const submitElement = document.getElementById('complete-btn');
 
 // Counter Elements
 const lineElement = document.getElementById('line');
@@ -19,26 +19,29 @@ const questionCounter = document.getElementById('questionCounter');
 const progressBar = document.getElementById('progressBar');
 
 // start Survey 
+const formSurveyPt1 = document.getElementById('formSurveyPt1');
+// survey Start button: 
 const startBtn = document.getElementById('start-btn');
 const startForm = document.getElementById('start-form');
 
 // End Questions 
+const endQuestionsContainer = document.getElementById('endQuestions');
 const eQ1 = document.getElementById('eQ1');
 const eQ2 = document.getElementById('eQ2');
 const eQ3 = document.getElementById('eQ3');
 
-// empty variable to hold 
+// set question index to 0
 let currentQuestionIndex = 0;
-
+// empty variable to hold selected answers as an object 
 let selectedAnswers = {};
 
+//validate email
 let validateEmail = (email) => {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
-// email.addEventListener('focus', validateEmail(email));
-//declare variables
-// add event listener
+
+// (A) START when user clicks the complete button
 startBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value.trim();
@@ -58,7 +61,7 @@ startBtn.addEventListener('click', (e) => {
 // after user clicks hide elements and show hidden elements
 // start survey 
 
-function startSurvey () {
+let startSurvey = () => {
     startForm.classList.add('hide');
     questionCounter.classList.remove('hide');
     progressBar.classList.remove('hide');
@@ -66,20 +69,37 @@ function startSurvey () {
     controls.classList.remove('hide');
 
     // step 1 //
-    setNextQuestion();
+    showFirstQuestion();
 }
 
-function setNextQuestion () {
+// show first question function at index 0
+let showFirstQuestion = () => {
+    showQuestion(questions[currentQuestionIndex]);
+    return;
+}
+// show question at index ==! 0
+let setNextQuestion = () => {
     // STEP 2 // 
     // STEP 5 // 
     resetState();
-    showQuestion(questions[currentQuestionIndex]);
-    }
 
-function showQuestion(question) {
+    if (questions.length > currentQuestionIndex) {
+        console.log('show next question confirmed');
+        showQuestion(questions[currentQuestionIndex]);
+    return;
+} else {
+    console.log('endSurvey() activate');
+    endSurvey();
+    return;
+ }
+}
+
+let showQuestion = (question) => {
     // STEP 3a //
-    console.log(question);
+    console.log('showQuestion', question);
     questionElement.innerText = question.question;
+    
+    
     question.answers.forEach(answer => {
         const answerButton = document.createElement('button');
         answerButton.innerText = answer.text;
@@ -90,9 +110,9 @@ function showQuestion(question) {
         answerButton.addEventListener('click', selectAnswer);
         answerButtonsElement.appendChild(answerButton);
     });
-}
+};
 
-function resetState() {
+let resetState = () => {
     // STEP 3b/5b //
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
@@ -100,75 +120,50 @@ function resetState() {
     nextButton.classList.add('opacity');
 }
 
-function selectAnswer (e) {
+let selectAnswer = (e) => {
     // STEP 4 //
-    const selectedButton = e.target;
-    const selectedText = selectedButton.dataset.text;
+    // set the question to a constant to be pushed to an object 
+    const questionKey = questions[currentQuestionIndex].question;
 
-    if (selectedText === selectedText) {
-        nextButton.classList.remove('opacity');
-        // if next is selected then add opacity and push selected text
+    // define the text of the btn that was selected
+    const selectedText = e.target.innerText;
+
+    nextButton.classList.remove('opacity');
+        // when next btn is clicked, push question/answer key/value to selectedAnswers object
         nextButton.addEventListener('click', () => {
-
-            const questionKey = questions[currentQuestionIndex].question;
-
-            // conditional logic if the selectedAnswer is updated...which... I can go back anyway
-            // for (let name in selectedAnswers) {
-            //     if (selectedAnswers.hasOwnProperty(name)) {
-
-            //     } 
-            // }
-            // if selectedAnswers.hasOwnProperty(keyValue) {
-
             selectedAnswers = {...selectedAnswers, [questionKey] : selectedText }
-            console.log(currentQuestionIndex);
+            console.log('selectedAnswers', selectedAnswers);
+            console.log('currentQuestionIndex before', currentQuestionIndex);
+    
+            // then increase the width of the progress bar
             switch (currentQuestionIndex) {
-                case 1:
-                    console.log('Q2');
+                case 0:
+                    console.log('Q1');
                     progressEl.style.width = '66%';
                     questionNumber.innerText = '2';
                     break;
-                case 2:
-                    console.log('Q3');
+                case 1:
+                    console.log('Q2');
                     progressEl.style.width = '80%';
                     questionNumber.innerText = '3';
-
+                    break;
+                case 2: 
+                    console.log('Q3');
+                    progressEl.style.width='90%';
+                    questionNumber.innerText='3';
                     break;
                 default:
-                    console.log(0);
+                    console.log('endQs');
                     break;
             }
-                currentQuestionIndex++;
-                console.log('Index [' + currentQuestionIndex + ']');
-                resetState();
-                setNextQuestion();
-            })
-    } else {
-        return;
-    }
+            currentQuestionIndex++;
+            console.log('currentQuestionIndex after', currentQuestionIndex);
+            console.log('questions.length', questions.length);
+            setNextQuestion();
+        }, { once: true });
+}
 
-    if (questions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove('hide');
-    } else {
-        nextButton.classList.add('hide');
-        backButton.classList.add('hide');
-        questionContainerElement.classList.add('hide');
-        progressEl.style.width = '100%';
-        eQ1.classList.remove('hide');
-        
-        submitElement.classList.remove('hide');
-        // have to change this... 
-    };
-};
-
-    backButton.addEventListener('click', () => {
-        let back = (currentQuestionIndex - 1)
-        console.log(back);
-        resetState();
-        showQuestion(questions[back]);
-    } );
-   
-function submitData(e) {
+let submitData = (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value.trim();
 
@@ -208,6 +203,22 @@ let surveyConfirmation = () => {
         window.parent.location.href = 'https://rentcalculator.com/thank-you-confirmation/';
     });
 };
+
+let endSurvey = () => {
+        formSurveyPt1.classList.add('hide');
+        endQuestionsContainer.classList.remove('hide');
+        eQ1.classList.remove('hide');
+        return;
+//and questions 
+    // show last questions
+    // add event listener on btn
+    // if true button is clicked then schedule
+    // if false is clicked show no problem and continue to push data
+    // then once it is clicked, push data and we only need this at the end so the even listener at the end gets put into this function.
+}
+
+// when survey is complete, console.log complete
+// => run fuction to hide all attributes and show eQ1
 
 const questions = [
     {
@@ -299,49 +310,21 @@ const questions = [
                 value: 20
             }
         ]
-    },
-    {
-        question: 'How Much Would You Be Willing To Put Down As A Down Payment Towards A New Home?',
-        answers: [
-            {
-                text: '$0',
-                correct: true,
-                value: 1
-            },
-            {
-                text: '$7,500',
-                correct: true,
-                value: 3
-            },
-            {
-                text: '$12,500',
-                correct: true,
-                value: 5
-            },
-            {
-                text: '$25,000',
-                correct: true,
-                value: 10
-            },
-            {
-                text: 'Over $25,000',
-                correct: true,
-                value: 20
-            }
-        ]
     }
 ]
 
-let endSurvey = () => {
-    // hide anwers and questions 
-    // show last questions
-    // create but
+backButton.addEventListener('click', () => {
+    let back = (currentQuestionIndex - 1)
 
-}
+    if (currentQuestionIndex !== 0 ) {
+    console.log(back);
+    resetState();
+    showQuestion(questions[back]);
+    } else {
+        console.log('cannot go back');
+        return;
+    }
+} );
 
 // startSurvey();
-
 submitElement.addEventListener('click', submitData);
-
-
-
