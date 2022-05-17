02c314d59fec20442203914c94ce11a4df685653
joshacross <joshuaalexandercross@gmail.com>
2022-05-17 01:47:23 -0500

    updated rentcalculator form

--- assets/js/rentcalc.js
+++ assets/js/rentcalc.js
@@ -1,74 +1,120 @@
+
+
 const multiply = (num1, num2) => {
     return Math.floor(num1*num2);
 }
 
 let formData = [];
 
-	
-function returnRent(e) {
-    e.preventDefault(); 
-	// e.stopPropagation(); e.stopImmediatePropagation();
-    const name = document.getElementById('et_pb_contact_name_0').value.trim();
-    const phone = document.getElementById('et_pb_contact_phone_0').value.trim();
-    const rent = document.getElementById('et_pb_contact_rent_0').value.trim();
+let getRentPayment = async (e) => {
+    e.preventDefault();
+    const rentPayment = document.getElementById('rent').value.trim();
+    console.log('rentPayment', rentPayment);
 
-    if ((name) || (phone) || (rent)) {
-
-    const pvResult = multiply(rent, 201.47766315152);
-    
-    let presentValue = document.createElement('input');
-    presentValue.value = pvResult;
-    presentValue.setAttribute('hidden', true);
-    let formEl = document.getElementById('form');
-    formEl.appendChild(presentValue);
-    console.log(presentValue);
-
-    let formData = {'userName': name, 'userPhone': phone, 'userRent': rent, 'userPV': presentValue.value};
-
-let setStorage = () => {
-    localStorage.setItem('pv', presentValue.value);
-    localStorage.setItem('name', name);
-    localStorage.setItem('phone', phone);
-    localStorage.setItem('rent', rent);
-}
-//Send Form Data
-
-async function sendForm (url='', data = {}) {
-
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'omit',
-        // headers: {
-        //     'Accept': 'application/json',
-        //     'Content-Type': 'application/json'
-        // },
-        // redirect: 'follow',
-        referrPolicy: 'strict-origin-when-cross-origin',
-        body: JSON.stringify(data)
-    });
-        return response.json();
+    if (!rentPayment) {
+       await console.log ('rentPayment is null');
+    } else {
+        console.log('rentPayment is not null')
     }
+};
 
-    setStorage();
+// add eventListener to form button
+
+const formDataElement = document.getElementById('form').addEventListener('submit', getRentPayment);
+
+// // wait for document to load
+
+// // once document loads, 
+
+	
+// function returnRent(e) {
+//     e.preventDefault(); 
+//     const rent = document.getElementById('rent').value.trim();
+//     let rentObject = { 'rent': rent };
+
+//     pushFormData = () => {
+//         let formDataPush = () => {formData.push.localStorage(rentObject);};
+
+//         console.log('localStorage', localStorage);
+
+//         if (rent) {
+//             console.log('rentObjAfter', rent);
+//         } else {
+//             console.log('rentObjectAfter is Null');
+//         }
+//         formDataPush();
+//     }
+
+// 	// e.stopPropagation(); e.stopImmediatePropagation();
+//     // const name = document.getElementById('et_pb_contact_name_0').value.trim();
+//     // // const phone = document.getElementById('et_pb_contact_phone_0').value.trim();
+//     // const rent = document.getElementById('et_pb_contact_rent_0').value.trim();
+
+
+// };
+// //     if ((name) || (phone) || (rent)) {
+
+// //     const pvResult = multiply(rent, 201.47766315152);
+    
+// //     let presentValue = document.createElement('input');
+// //     presentValue.value = pvResult;
+// //     presentValue.setAttribute('hidden', true);
+// //     let formEl = document.getElementById('form');
+// //     formEl.appendChild(presentValue);
+// //     console.log(presentValue);
+
+// //     let formData = {'userName': name, 'userPhone': phone, 'userRent': rent, 'userPV': presentValue.value};
+
+// // let setStorage = () => {
+// //     localStorage.setItem('pv', presentValue.value);
+// //     localStorage.setItem('name', name);
+// //     localStorage.setItem('phone', phone);
+// //     localStorage.setItem('rent', rent);
+// // }
+// // //Send Form Data
+
+// // async function sendForm (url='', data = {}) {
+
+// //     const response = await fetch(url, {
+// //         method: 'POST',
+// //         mode: 'cors',
+// //         cache: 'no-cache',
+// //         credentials: 'omit',
+// //         // headers: {
+// //         //     'Accept': 'application/json',
+// //         //     'Content-Type': 'application/json'
+// //         // },
+// //         // redirect: 'follow',
+// //         refererPolicy: 'strict-origin-when-cross-origin',
+// //         body: JSON.stringify(data)
+// //     });
+// //         return response.json();
+// //     }
+
+// //     setStorage();
 
   
-sendForm('https://hooks.zapier.com/hooks/catch/9671423/b8om3hn/', formData)
-    .then(data => {
-        console.log(data);
-    }).catch((err) => {
-        console.log(err);
-    });
-    window.location.href = "https://rentcalculator.com/properties/?widget_id=2&kind=0&sf_unit_price=260&sf_min_price=0&sf_max_price="+formData.userPV;
-} else {
-    console.log('must enter a value');
-    return;
-    }
-}
+// // sendForm('https://hooks.zapier.com/hooks/catch/9671423/b8om3hn/', formData)
+// //     .then(data => {
+// //         console.log(data);
+// //     }).catch((err) => {
+// //         console.log(err);
+// //     });
+// //     window.location.href = "https://rentcalculator.com/properties/?widget_id=2&kind=0&sf_unit_price=260&sf_min_price=0&sf_max_price="+formData.userPV;
+// // } else {
+// //     console.log('must enter a value');
+// //     return;
+// //     }
 
-document.addEventListener('DOMContentLoaded', function(){
-	formElement = document.getElementById('form').getElementsByTagName("form");
-	//console.log('DOMContentLoaded');
-	formElement[0].addEventListener('submit', returnRent); 
-});
+// pullLocal();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     setTimeout (() => {
+//         formElement = document.getElementById('form').getElementsByTagName('form');
+//         console.log('delayed for 5 seconds');
+//     }, '5000' );
+// 	//console.log('DOMContentLoaded');
+// 	formElement[0].addEventListener('submit', returnRent); 
+// });
+
+
