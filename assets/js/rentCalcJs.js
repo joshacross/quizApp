c4d1e5d72ddf06bb5a0318dbb58c4152dfee54d6
joshacross <joshuaalexandercross@gmail.com>
2022-05-19 11:56:21 -0500

    updating and fixing bugs associated with rentForm.html

--- assets/js/rentCalcJs.js
+++ assets/js/rentCalcJs.js
@@ -1,42 +1,33 @@
+let formData = [];
+
 const multiply = (num1, num2) => {
     return Math.floor(num1*num2);
 }
 
+
+
 // Get the modal
 const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+// Get the button that opens the modal but also will create obj and save to localStorage
 const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+const verifyBtn = document.getElementById('verify');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+const phoneVerificationBtn = document.getElementById('phoneVerificationBtn');
 
-let formData = [];
+const verificationContainer = document.getElementById('verificationContainer');
+
 
 	
-let returnRent = (e) => {
-    e.preventDefault();
-	// e.stopPropagation(); e.stopImmediatePropagation();
+let returnRent = () => { 
     const firstName = document.getElementById('firstName').value.trim();
     const lastName = document.getElementById('lastName').value.trim();
-    const rent = document.getElementById('et_pb_contact_rent_0').value.trim();
+    const rent = document.getElementById('rent').value.trim();
     const pvResult = multiply(rent, 150.47766315152);
 
     let formData = {'first_name': firstName, 'last_name': lastName, 'rentPayment': rent, 'principle': pvResult};
@@ -51,33 +42,15 @@ let returnRent = (e) => {
         setStorage();
         return formData;
     }
+ 
 
-let showVerificationInput = (e) => {
-    e.preventDefault();
-    btn.setAttribute('hidden', true);
-    let verificationNumber = document.getElementById('phoneVerificationNumber');
-    const phoneVerificationBtn = document.getElementById('phoneVerificationBtn');
-    phoneVerificationBtn.addEventListener('submit', phoneVerified);
-    verificationNumber.setAttribute('hidden', false);
-    phoneVerificationBtn.setAttribute('hidden', false);
-}
-
-let phoneVerified = (e) => {
-    e.preventDefault();
-    const phone = document.getElementById('phone').value.trim();
-
-    let formData = { ...formData, 'phone': phone };
-
-    let setStorage = () => {
-        localStorage.setItem('phone', phone);
-    }
-
-    setStorage();
+let sendData = (e) => {
     
-    /////////TWILIO VERIFICATION INSERT ///////////
-    document.getElementById('verify');
-    if (twilioVerifiedComplete) {
+    /////////TWILIO VERIFICATION CALLBACK ///////////
+        e.preventDefault();
+
         
+        console.log('sendData() initiated');
         async function sendForm (url='', data = {}) {
 
             const response = await fetch(url, {
@@ -90,7 +63,7 @@ let phoneVerified = (e) => {
                 //     'Content-Type': 'application/json'
                 // },
                 // redirect: 'follow',
-                referrPolicy: 'strict-origin-when-cross-origin',
+                refererPolicy: 'strict-origin-when-cross-origin',
                 body: JSON.stringify(data)
             });
                 return response.json();
@@ -106,19 +79,48 @@ let phoneVerified = (e) => {
                 console.log(err);
             });
             window.location.href = "https://rentcalculator.com/properties/?widget_id=2&kind=0&sf_unit_price=260&sf_min_price=0&sf_max_price="+formData.userPV;
-        } else {
-            console.log('must enter a value');
-            return;
-            }
+
+}
+
+
+btn.onclick = () => {
+    modal.style.display = "block";
+
+};
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = (e) => {
+    e.preventDefault();
+    returnRent();
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = () => {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  verifyBtn.addEventListener('click', ()=> {
+    console.log('verifyBtn clicked');
+    
+    const phone = document.getElementById('phone').value.trim();
+    
+        formData = { ...formData, 'phone': phone };
+    
+        let setStorage = () => {
+            localStorage.setItem('phone', phone);
         }
     
-//Send Form Data
-
-document.addEventListener('DOMContentLoaded', () => {
-	const formElement = document.getElementById('form');
-	console.log(formElement);
-    formElement.addEventListener('submit', returnRent);
-    const verify = document.getElementById('verify');
-    verify.addEventListener('click', showVerificationInput);
-	// formElement.addEventListener('submit', returnRent); 
+        setStorage();
+    phoneVerificationBtn.addEventListener('submit', sendData);
 });
+
+    
