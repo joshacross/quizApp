ac200830b49e85ad537f5f668ac19bff037deabc
joshacross <joshuaalexandercross@gmail.com>
2022-05-21 00:29:00 -0500

    updating security permissions

--- assets/js/rentCalcJs.js
+++ assets/js/rentCalcJs.js
@@ -11,41 +11,6 @@ const multiply = (num1, num2) => {
 }
 //calculate button
 let calculateBtn = document.getElementById('calculateBtn');
-	
-// let loadStorage = () => {
-//     principle = localStorage.getItem('principle');
-//     firstName =  localStorage.getItem('first_name');
-//     lastName = localStorage.getItem('last_name');
-//     phone = localStorage.getItem('phone');
-//     rent = localStorage.getItem('rent');
-
-//     if ( principle !== null || firstName !==null || lastName !== null || phone !== null || rent !== null && rent !== NaN ) {
-//         let formData = {
-//             'first_name': firstName, 
-//             'last_name': lastName, 
-//             'phone': phone, 
-//             'rentPayment': rent, 
-//             'principle': principle
-//         }
-
-//         const firstNameEl = document.getElementById('firstName');
-//         const lastNameEl = document.getElementById('lastName');
-//         const phoneEl = document.getElementById('phone');
-//         const rentEl = document.getElementById('rent');
-    
-//         firstNameEl.value = firstName;
-//         lastNameEl.value = lastName
-//         phoneEl.value = phone
-//         rentEl.value = rent
-    
-//         return (formData);
-//     } else {
-//         console.log('no storage loaded');
-//     }
-
-
-
-// }
 
 // After calculateBtn is clicked: 
 let returnRent = () => { 
@@ -86,19 +51,7 @@ let setStorage = () => {
 
 // after verifyPhoneBtn is clicked
 let sendTextConfirmation = () => {
-    // const phone = document.getElementById('phone').value.trim();
-    // let phoneID = '+' + '1' + phone;
-    // console.log(phoneID);
-
-    // //////// TWILIO INFORMATION WEBHOOK AND ENVIRONMENT VARIABLES
-    // const accountSid = process.env.TWILIO_ACCOUNT_SID;
-    // const authToken = process.env.TWILIO_AUTH_TOKEN;
-    // const client = require('twilio')(accountSid, authToken);
-    
-    // client.verify.services('VA74b70157d8fcec820e82ec4f9fb81125')
-    //              .verifications
-    //              .create({to: + phoneID , channel: 'sms'})
-    //              .then(verification => console.log(verification.sid));
+//Insert Twilio Here
     console.log('twilio text-confirmation');
     return;
     
