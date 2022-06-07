83b8acd512bbc8cc998f5144d0549a88664d51e9
joshacross <joshuaalexandercross@gmail.com>
2022-06-07 01:34:06 -0500

    added the rental cost algorithm

---
+++ assets/js/rentCost.js
@@ -0,0 +1,57 @@
+let P = 3000
+let total = Number;
+
+let arr = [P];
+
+let arrayLoop = () => {
+    for (let i = 0; i < 29; i++) {
+
+            let rate = .03875;
+            let payment = (P+(P*rate));
+            P = payment;
+            console.log('payment', P);
+            arr.push(payment);
+            console.log('array', arr);
+      }
+
+    const sum = arr.reduce((accumulator, value) => {
+        return accumulator + value;
+    }, 0);
+
+    console.log('sum', sum)
+    total = sum*12;
+    console.log('total', total)
+    return total;
+}
+
+showTableOnPage = () => {
+	let arrayContainer = document.getElementById("container").textContent 
+    
+    arrayContainer = arr.join(", ");
+    let title = document.createElement('h1');
+    title.textContext = total;
+    arrayContainer.appendChild('title');
+  }
+//   //append the compiled table to the DOM
+//   document.body.appendChild(table);
+//     //array = arr
+
+//   //create a Table Object
+//   let tableEl = document.createElement('table');
+//   for (let row of arr) {
+//     tableEl.insertRow();
+//     for (let cell of row) {
+//       let newCell = tableEl.rows[tableEl.rows.length - 1].insertCell();
+//       newCell.textContent = cell;
+//     }
+//   }
+//   document.body.appendChild(tableEl);
+
+
+
+
+
+arrayLoop();
+showTableOnPage();
+
+// (D) ATTACH TABLE TO CONTAINER
