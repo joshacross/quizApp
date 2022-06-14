5c4923dc67661e59720d9240e479ddba188e0345
joshacross <joshuaalexandercross@gmail.com>
2022-06-13 23:11:47 -0400

    refactored and fixed table data to show on page

--- assets/js/rentCost.js
+++ assets/js/rentCost.js
@@ -1,42 +1,88 @@
-let P = 3000
+const tableEl = document.getElementById('table-data');
+let P = 3000;
+let rate = .02;
 let total = Number;
-
 let arr = [P];
+let calendar = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sept', 'oct', 'nov', 'dec'];
+
 
 let arrayLoop = () => {
-    for (let i = 0; i < 29; i++) {
-
-            let rate = .03875;
+    for (let i = 1; i <= 29; i++) {
+            // create table row
+            let tableRowEl = document.createElement('tr');
+            let yearEl = document.createElement('td');
+            yearEl.textContent = 'year' + i;
+            tableRowEl.appendChild(yearEl);
+            tableEl.appendChild(tableRowEl);
+            // 
             let payment = (P+(P*rate));
             P = payment;
             console.log('payment', P);
-            arr.push(payment);
-            console.log('array', arr);
+            arr.push(P);
+            console.log('arr', arr);
+            calendar.forEach(() => {
+              let tableData = document.createElement('td');
+              tableData.textContent = '$' + Math.round(arr[i]).toLocaleString();
+              tableRowEl.appendChild(tableData);
+              // tableDataContainer.appendChild(tableDataDiv);
+            });
+            let totalEl = document.createElement('td');
+            totalEl.textContent = '$' + Math.round(arr[i]*12).toLocaleString();
+            tableRowEl.appendChild(totalEl);
       }
 
     const sum = arr.reduce((accumulator, value) => {
         return accumulator + value;
     }, 0);
 
-    console.log('sum', sum)
-    total = sum*12;
-    console.log('total', total)
-    return total;
+    let grandTotalRowEl = document.createElement('tr');
+    let grandTotalStringEl = document.createElement('td');
+    grandTotalStringEl.textContent = 'Total = ';
+    let grandTotalDataEl = document.createElement('td');
+    grandTotalDataEl.textContent = '$' + Math.round(sum*12).toLocaleString();
+    grandTotalRowEl.appendChild(grandTotalStringEl);
+    grandTotalRowEl.appendChild(grandTotalDataEl);
+    tableEl.appendChild(grandTotalRowEl);
+
 }
 
-showTableOnPage = () => {
+// showTableOnPage = () => {
+//   for (let i = 0; i<calender.length; i++) {
+//     let calendarEl = document.createElement('td');
+//     let calendarDiv = document.createElement('div');
+//   }
+// }
 
-    const containerEl = document.getElementById('container');
-    let divContainerEl = document.createElement('div');
 
-    let arrayData = document.createElement('h6');
+// showTableOnPage = () => {
+//   const janEl = document.getElementById('jan');
+//   const febEl = document.getElementById('feb');
+//   const marEl = document.getElementById('mar');
+//   const aprEl = document.getElementById('apr');
+//   const mayEl = document.getElementById('may');
+//   const junEl = document.getElementById('jun');
+//   const julEl = document.getElementById('jul');
+//   const augEl = document.getElementById('aug');
+//   const septEl = document.getElementById('sept');
+//   const octEl = document.getElementById('oct');
+//   const novEl = document.getElementById('nov');
+//   const decEl = document.getElementById('dec');
     
-    arrayData = arr.join(", ");
-    let title = document.createElement('h1');
-    title.textContext = total;
-    divContainer = document.createElement('div');
-    divContainer.appendChild(title);
-  }
+//     for (let i = 0; i <=29; i++) {
+//       janEl.textContent = arr[i];
+//       febEl.textContent = arr[i];
+//       marEl.textContent = arr[i];
+//       aprEl.textContent = arr[i];
+//       mayEl.textContent = arr[i];
+//       junEl.textContent = arr[i];
+//       julEl.textContent = arr[i];
+//       augEl.textContent = arr[i];
+//       septEl.textContent = arr[i];
+//       octEl.textContent = arr[i];
+//       novEl.textContent = arr[i];
+//       decEl.textContent = arr[i];
+//     }
+//   }
 //   //append the compiled table to the DOM
 //   document.body.appendChild(table);
 //     //array = arr
@@ -57,6 +103,6 @@ showTableOnPage = () => {
 
 
 arrayLoop();
-showTableOnPage();
+// showTableOnPage();
 
 // (D) ATTACH TABLE TO CONTAINER
